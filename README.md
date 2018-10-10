1049557b772da9c76c2b20b8fe254b4e8ba48322
李文浩 <wenhao.li08@ele.me>
2018-10-10 19:44:07 +0800

    screenshot-puppeteer.js

---
+++ README.md
@@ -0,0 +1,9 @@
+# 代码集合
+
+# javascript
+
+javascript/screenshot-puppeteer.js
+
+使用puppeteer截图，通过滑动到底部来解决图片的懒加载问题。
+
+
---
+++ javascript/screenshot-puppeteer.js
@@ -0,0 +1,42 @@
+const puppeteer = require('/usr/local/lib/node_modules/puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.zhihu.com/question/22263777');
+    // await page.goto('http://www.iqiyi.com');
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });
+
+    await autoScroll(page);
+
+    await page.screenshot({
+        path: '1.png',
+        fullPage: true
+    });
+
+    await browser.close();
+})();
+
+
+function autoScroll(page) {
+    return page.evaluate(() => {
+        return new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+                if (totalHeight >= scrollHeight) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        })
+    });
+}
